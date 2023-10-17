0b64270d52694ccd2dcc5e00b347ed4cc9b2858e
Gavriel Linoy <71229809+Gavriel770U@users.noreply.github.com>
2023-10-18 01:22:14 +0300

    Add files via upload

---
+++ install_linux_macos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip3 install tk
+pip3 install customtkinter
+pip3 install python_weather
+pip3 install requests
---
+++ install_windows.bat
@@ -0,0 +1,4 @@
+pip install tk
+pip install customtkinter
+pip install python_weather
+pip install requests
